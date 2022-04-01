dd02ed56692c9560e94ddd2eee4842fdfa37626d
meanstack <meanstack4@gmail.com>
2022-04-01 14:36:30 +0530

    add jsonwebtoken

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../model/user');
+const jwt = require('jsonwebtoken');
 // http://localhost:3000/singup
 // request:-{username: '',email: '',password: ''}
 router.post("/signin",(request,response)=>{
    User.findOne({email: request.body.email,password: request.body.password})
    .then(result=>{
-     if(result)
-      return response.status(200).json({status: 'loging success', current_user: result});
+     if(result){
+      let payload = {subject: result._id};
+      let token = jwt.sign(payload,'fdfdfdfrerererevcvcvcvcv');
+      return response.status(200).
+       json({ 
+             status: 'loging success',
+             current_user: result,
+             token: token
+            });
+     }
      else
       return response.status(200).json({status: 'login failed'}); 
    })
@@ -15,6 +24,7 @@ router.post("/signin",(request,response)=>{
      return response.status(500).json({error: 'Internal server error'});
    });
 });
+
 router.post("/signup",(request,response)=>{
    User.create({
      username: request.body.username,

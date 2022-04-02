809e5e7df909ac19f9a25220f91b1f8f07dbeedd
meanstack <meanstack4@gmail.com>
2022-04-02 12:27:08 +0530

    delete category api

--- routes/category.route.js
+++ routes/category.route.js
@@ -4,6 +4,15 @@ const Category = require('../model/category');
 // http://localhost:3000/category/add-categort
 // request-{categoryName: ''}
 const tokenVerification = require('../middleware/token_verification');
+
+router.post("/delete-category",tokenVerification.verifyToken,(request,response)=>{
+   Category.deleteOne({_id: request.body.cid})
+   .then(result=>{
+      return response.status(200).json({status: 'success'});
+   }).catch(err=>{
+       return response.status(500).json({error: 'Internal server error'});
+   });
+});
 router.get("/category-list",tokenVerification.verifyToken,(request,response)=>{
   Category.find()
   .then(result=>{
